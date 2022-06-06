b38983429dde2e264e67c421447a2cc7f248c688
Azriel-ds <106836790+Azriel-ds@users.noreply.github.com>
2022-06-06 22:08:00 +0530

    Add files via upload

--- Assignment 2-Extract file extention.py
+++ Assignment 2-Extract file extention.py
@@ -1,14 +1,7 @@
-File_extention = ['py', 'c', 'c++']
-Filename=input("Input the Filename: ")
-for i in File_extention:
-    if (Filename=='py'):
-        print("The extention of the file is: Python")
-        break
-    elif (Filename=='c'):
-       print("The extention of the file is: C")
-       break
-    elif(Filename=='c++'):
-        print("The extention of the file is: C++")
-        break
-    else:
-        print("Invalid Extention")
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+if f_extns[-1]=='py':
+    print("The extension of the file is : Python")
+elif f_extns[-1]=='c':
+    print("The extension of the file is : C")
+
